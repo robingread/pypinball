32789698150de08964e32c864473ffd3f414ec26
Robin Read <robin.g.read@gmail.com>
2022-04-17 21:13:10 +0100

    scripts: Updated setup-venv.sh to check whether virtualenv is already installed, and if not, install it.

--- scripts/setup-venv.sh
+++ scripts/setup-venv.sh
@@ -2,8 +2,15 @@
 
 set -e
 
-DST_DIR=./venv
+if ! which virtualenv &> /dev/null
+then
+  echo "Installing virtual env..."
+  pip install virtualenv
+else
+  echo "Virtualenv found and already installed"
+fi
 
+DST_DIR=./venv
 virtualenv --clear --python /usr/bin/python3 $DST_DIR
 
 source $DST_DIR/bin/activate
