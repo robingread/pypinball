5ac5ee46d363fab28eb3989324bb5667f254c40f
Robin Read <robin.g.read@gmail.com>
2022-04-19 14:00:32 +0100

    pypinball: Updated audio/sounds.py to include a BALL_LAUNCH sound enum value.

--- pypinball/audio/sounds.py
+++ pypinball/audio/sounds.py
@@ -6,6 +6,7 @@ class Sounds(enum.Enum):
     Define the different types of sound that are defined within the Game.
     """
 
+    BALL_LAUNCH = enum.auto()
     FLIPPER_ACTIVATE = enum.auto()
     FLIPPER_BALL_BOUNCE = enum.auto()
     GAME_START = enum.auto()
