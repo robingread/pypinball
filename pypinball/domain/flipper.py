f490475748f78b780d6a20f027aa24d84d45b378
Robin Read <robin.g.read@gmail.com>
2022-05-01 21:20:47 +0100

    pypinball: Updated the FlipperConfig class in flipper.py to include new properties. Specifically these are the rest_angle, actuation_angle, actuation_button and the actuation_direction.

--- pypinball/domain/flipper.py
+++ pypinball/domain/flipper.py
@@ -1,12 +1,26 @@
 import dataclasses
+from .buttons import Buttons
 
 
 @dataclasses.dataclass(frozen=True)
 class FlipperConfig:
+    """
+    Flipper configuration dataclass. Flippers have the following properties:
+
+    - position (tuple): Position in the format (x, y).
+    - length (float): Length of the flipper arm.
+    - rest_angle (float):
+    - actuation_angle (float):
+    - actuation_button (Buttons): Button that actuates the flipper.
+    - actuation_direction (int): Direction the flipper actuates in. 1 corresponds to counter clockwise and -1 clockwise.
+    """
+
     position: tuple
     length: float
-    min_angle: float
-    max_angle: float
+    rest_angle: float
+    actuation_angle: float
+    actuation_button: Buttons
+    actuation_direction: int
 
 
 class Flipper:
