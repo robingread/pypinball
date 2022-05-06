feed32e9b520de2a2d6157c285cd19a69a4234b4
Robin Read <robin.g.read@gmail.com>
2022-05-06 17:46:47 +0100

    pypinball: Updated the add_ball() method in the PhysicsInterface class return a bool value.

--- pypinball/physics/physics_interface.py
+++ pypinball/physics/physics_interface.py
@@ -9,7 +9,7 @@ class PhysicsInterface(typing.Protocol):
     def actuate_flippers(self, actuate_button: domain.Buttons) -> None:
         ...
 
-    def add_ball(self, ball: domain.Ball) -> None:
+    def add_ball(self, ball: domain.Ball) -> bool:
         ...
 
     def add_flipper(self, flipper: domain.Flipper) -> bool:
