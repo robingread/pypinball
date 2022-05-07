b9084567b3d82300c3b5f43102ff6c25bce816cd
Robin Read <robin.g.read@gmail.com>
2022-05-07 16:26:04 +0100

    test: Added testing for launching balls in PymunkPhysics in the test_ball_physics.py test script.

--- test/physics/test_ball_physics.py
+++ test/physics/test_ball_physics.py
@@ -44,3 +44,31 @@ class TestBallGeneration(unittest.TestCase):
         """
         with self.assertRaises(KeyError):
             self.physics.get_ball_state(uid=10)
+
+
+class TestBallLaunch(unittest.TestCase):
+    """
+    Test the general functionality of launching a ball via the PymunkPhysics class.
+    """
+
+    def setUp(self) -> None:
+        self.ball = pypinball.domain.Ball(uid=0, position=(100, 100))
+        self.physics = pypinball.physics.PymunkPhysics()
+        self.physics.add_ball(ball=self.ball)
+
+    def test_launch_ball(self):
+        """
+        Test that after being launched, the ball position has changed from the
+        initial position when it was created, and it is moving against the
+        gravity vector.
+        """
+        initial_state = self.physics.get_ball_state(uid=self.ball.uid)
+        self.assertTupleEqual(self.ball.position, initial_state.position)
+
+        self.physics.launch_ball(uid=self.ball.uid)
+
+        for _ in range(100):
+            self.physics.update()
+
+        new_state = self.physics.get_ball_state(uid=self.ball.uid)
+        self.assertLessEqual(new_state.position[1], initial_state.position[1])
