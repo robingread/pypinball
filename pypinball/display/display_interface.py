ef1d8894a0ed0830609bc9fae4e7007626042879
Robin Read <robin.g.read@gmail.com>
2022-05-25 22:41:38 +0100

    pypinball: Updated the doc-strings for the DisplayInterface class.

--- pypinball/display/display_interface.py
+++ pypinball/display/display_interface.py
@@ -3,16 +3,16 @@ import typing
 
 class DisplayInterface(typing.Protocol):
     def clear(self) -> None:
-        ...
+        """ """
 
     def display_image(self, path: str) -> None:
-        ...
+        """ """
 
     def draw_ball(self, pos: list, radius: int) -> None:
-        ...
+        """ """
 
     def draw_flipper(self) -> None:
-        ...
+        """ """
 
     def update(self) -> None:
-        ...
+        """ """
