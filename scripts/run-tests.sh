af207edb433024dcc7c3337e59ddad5cb6e91b89
Robin Read <robin.g.read@gmail.com>
2022-05-25 22:32:17 +0100

    scripts: Updated run-test.sh script to show missing code coverage.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -6,5 +6,5 @@ echo "Running Python unittests & calculating code coverage..."
 
 pip install -q .
 coverage run --data-file=coverage/.coverage -m pytest -q test
-coverage report --data-file=coverage/.coverage
+coverage report -m --data-file=coverage/.coverage
 coverage xml -q --data-file=coverage/.coverage -o coverage/coverage.xml
