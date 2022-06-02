c499c34591e3c3c8157cbdd5801e1dfd238ccde2
Robin Read <robin.g.read@gmail.com>
2022-06-02 21:45:42 +0100

    test: Added the moc_interfaces.py module in the test folder.

---
+++ test/moc_interfaces.py
@@ -0,0 +1,50 @@
+import pypinball
+import typing
+
+
+class MocAudio(pypinball.AudioInterface):
+    """
+    Moc AudioInterface that stores the sounds which have been requested without
+    actually playing any audio.
+    """
+
+    def __init__(self):
+        self._sounds = list()
+
+    @property
+    def sounds(self) -> set:
+        return set(self._sounds)
+
+    def play_sound_file(self, file_path: str) -> bool:
+        self._sounds.append(file_path)
+        return True
+
+
+class MocDisplayInterface(pypinball.DisplayInterface):
+    def clear(self) -> None:
+        pass
+
+    def display_image(self, path: str) -> None:
+        pass
+
+    def update(self) -> None:
+        pass
+
+
+class MocInputInterface(pypinball.InputInterface):
+    """
+    Moc input interface which allows users to specify the input state that
+    should be returned.
+    """
+
+    def __init__(self, center_button=False, left_button=False, right_button=False):
+        self._center = center_button
+        self._left = left_button
+        self._right = right_button
+
+    def get_input_state(self) -> typing.Dict[pypinball.domain.Buttons, bool]:
+        return {
+            pypinball.domain.Buttons.CENTER: self._center,
+            pypinball.domain.Buttons.LEFT: self._left,
+            pypinball.domain.Buttons.RIGHT: self._right,
+        }
