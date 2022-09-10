6fd34c5e750ae81594cf1f76df623bcf432361f1
Robin Read <robin.g.read@gmail.com>
2022-09-10 18:20:54 +0100

    pypinball: Updated docstrings for the AudioInterface class in the audio.audio_interface.py module.

--- pypinball/audio/audio_interface.py
+++ pypinball/audio/audio_interface.py
@@ -3,9 +3,13 @@ import typing
 
 class AudioInterface(typing.Protocol):
     """
-    Audio Interface protocol definition. This is an interface class and does not implement any actual behaviour.
-    Concrete implementations of audio interfaces should either inherit from this class or implement the same
+    Audio Interface protocol definition. This is an interface class and does
+    not implement any actual behaviour. Concrete implementations of audio
+    interfaces should either inherit from this class or implement the same
     methods.
+
+    The AudioInterface is used to play audio files which are specified via a
+    full path to a local file (e.g. MP3, WAV).
     """
 
     def play_sound_file(self, file_path: str) -> bool:
