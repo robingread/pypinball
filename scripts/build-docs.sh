f0396481f2b096f3a3d972fd46c27337e748e611
Robin Read <robin.g.read@gmail.com>
2022-09-14 22:18:20 +0100

    scripts: Updated build-docs.sh to include (re-)installing pypinball before building docs. This is needed on CI side to be able to generate documentation.

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -3,4 +3,7 @@
 set -e
 
 echo "Building docs..."
+pip install -q --ignore-installed .
+rm -rf docs/build/*
+sphinx-apidoc -f -o docs/source/apidoc pypinball/
 sphinx-build docs/source docs/build/public
