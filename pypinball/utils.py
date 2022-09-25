6caf31c59655cdb49821dbee8d829351ee9c066d
Robin Read <robin.g.read@gmail.com>
2022-09-25 18:21:42 +0100

    pypinball: Removed the utils.actuate_flippers() method and the unit-tests in the test_utils.py module.

--- pypinball/utils.py
+++ pypinball/utils.py
@@ -10,29 +10,6 @@ BUTTON_SOUND_MAP = typing.Dict[Buttons, Sounds]
 SOUND_FILE_MAP = typing.Dict[Sounds, str]
 
 
-def actuate_flippers(
-    input_state: INPUT_STATE,
-    flippers: typing.List[Flipper],
-    physics: PhysicsInterface,
-) -> None:
-    """
-    Actuate flippers in the Physics model based upon the button input state.
-
-    Args:
-        input_state (dict): Dictionary where keys are ``Button`` enums and the value is a ``bool``.
-        flippers (list): List of flippers in game.
-        physics (PhysicsInterface): Concrete physics interface.
-
-    Returns:
-        None
-    """
-    pressed_buttons = [button for button, state in input_state.items() if state]
-
-    for flipper in flippers:
-        if flipper.config.actuation_button in pressed_buttons:
-            physics.actuate_flipper(uid=flipper.uid)
-
-
 def check_ball_is_within_area(
     ball_position: typing.Tuple[float, float], width: float, height: float
 ) -> bool:
