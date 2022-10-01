a2860c82d6290781e564162bab3b05240ac632b3
Robin Read <robin.g.read@gmail.com>
2022-10-01 17:08:06 +0100

    pypinball: Updated the simpleaudio_interface.py module to use the log.py module.

--- pypinball/audio/simpleaudio_interface.py
+++ pypinball/audio/simpleaudio_interface.py
@@ -1,7 +1,9 @@
-import logging
 import os
 import simpleaudio
 from .audio_interface import AudioInterface
+from .. import log
+
+logger = log.get_logger(name=__name__)
 
 
 class SimpleAudio(AudioInterface):
@@ -9,10 +11,10 @@ class SimpleAudio(AudioInterface):
         self._blocking = blocking
 
     def play_sound_file(self, file_path: str) -> bool:
-        logging.debug(f"Playing sound file: {file_path}")
+        logger.debug(f"Playing sound file: {file_path}")
 
         if not os.path.isfile(file_path):
-            logging.warning(f"Unknown find sound file: {file_path}")
+            logger.warning(f"Unknown find sound file: {file_path}")
             return False
 
         wave_obj = simpleaudio.WaveObject.from_wave_file(file_path)
