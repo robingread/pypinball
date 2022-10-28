fbe6efb5f56df9c6d19fed82bc2c46115b231825
Robin Read <robin.g.read@gmail.com>
2022-10-28 17:44:41 +0100

    pypinball: Removed the collisions.py module from the domain package as it is obsolete.

--- pypinball/domain/__init__.py
+++ pypinball/domain/__init__.py
@@ -1,5 +1,4 @@
 from .ball import Ball, BallState
 from .bumper import Bumper, RectangleBumper, RoundBumper
-from .collisions import Collision, CollisionType
 from .flipper import Flipper, FlipperConfig, FlipperState
 from .wall import Wall
