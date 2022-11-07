6251f536860746450932bc5ee78dedfec73b8409
Robin Read <robin.g.read@gmail.com>
2022-11-07 19:05:42 +0000

    pypinball: Updated setup.py to install the PNG resource files.

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     name="pypinball",
     version="0.0.1a",
-    packages=find_packages(include=['pypinball', 'pypinball.*']),
-    package_data={"pypinball.resources.audio": ["*.wav"]},
+    packages=find_packages(include=["pypinball", "pypinball.*"]),
+    package_data={
+        "pypinball.resources.audio": ["*.wav"],
+        "pypinball.resources.images": ["*.png"],
+    },
 )
