1e8ab3f835683340d9e7f57bd8445d2644aa3935
Robin Read <robin.g.read@gmail.com>
2022-11-07 18:36:18 +0000

    pypinball: Updated doc-strings in the resource.py module.

--- pypinball/resources/resource.py
+++ pypinball/resources/resource.py
@@ -8,7 +8,7 @@ logger = log.get_logger(name=__name__)
 def get_python_pkg_resource_path(prefix: str, resource: str) -> str:
     """
     Get the full system path of a resource file within a python package. For
-    example, loading a resource at some.pkg.module.audio.wav.
+    example, loading a resource at ``some.pkg.module.audio.wav``.
 
     Args:
         prefix (str): Python module path in dot format (e.g. some.pkg.module).
@@ -16,6 +16,10 @@ def get_python_pkg_resource_path(prefix: str, resource: str) -> str:
 
     Returns:
         str: Full system path of a resource.
+
+    Raises:
+        ModuleNotFoundError: If the prefix does not exist.
+        FileNotFoundError: If the resource file does not exist.
     """
     logger.debug(f"Loading resource path, prefix: {prefix}, resource: {resource}")
     with importlib.resources.path(package=prefix, resource=resource) as path:
@@ -25,13 +29,16 @@ def get_python_pkg_resource_path(prefix: str, resource: str) -> str:
 def get_audio_resource_path(filename: str) -> str:
     """
     Get the full system path for an audio resource stored within the pypinball
-    package under the resources.audio module.
+    package under the ``pypinball.resources.audio`` module.
 
     Args:
         filename (str): Name of the resource in the resources.audio module.
 
     Returns:
         str: Full system path of the resource.
+
+    Raises:
+        FileNotFoundError: If the resource filename doesn't exist.
     """
     prefix = "pypinball.resources.audio"
     return get_python_pkg_resource_path(prefix=prefix, resource=filename)
