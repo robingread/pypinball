d3c5a94ff96095af75b445f568b83abaaf7716c4
Robin Read <robin.g.read@gmail.com>
2022-11-30 22:38:31 +0000

    scripts: Updated the scripts/linting.sh script to use pylint-exit to only cause a script failure when an error is raised.

--- scripts/linting.sh
+++ scripts/linting.sh
@@ -2,4 +2,10 @@
 
 set -e
 source venv/bin/activate
-pylint --rcfile .pylintrc pypinball || true
+
+pylint --verbose --rcfile .pylintrc pypinball || pylint-exit --error-fail $?
+
+if [ $? -ne 0 ]; then
+  echo "An error occurred while running pylint." >&2
+  exit 1
+fi
