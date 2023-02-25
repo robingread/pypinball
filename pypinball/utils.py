c38805965b0b7e493d3f7cdcc393611516edc98a
Robin Read <robin.g.read@gmail.com>
2023-02-25 18:08:03 +0000

    pypinball: Fixed minor bug in the pypinball.utils.render_phyiscs_balls() method to double the radius value as the DisplayInterface.draw_ball() method needs a diameter, not a radius value.

--- pypinball/utils.py
+++ pypinball/utils.py
@@ -36,7 +36,7 @@ def render_physics_balls(
         display (DisplayInterface): Display to draw the balls onto.
     """
     for ball in balls:
-        display.draw_ball(pos=ball.position, diameter=ball.radius, alpha=1.0)
+        display.draw_ball(pos=ball.position, diameter=ball.radius * 2.0, alpha=1.0)
 
 
 def render_physics_bumpers(
