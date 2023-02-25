1cdb6be2ac662db37041986d0070402bfde91399
Robin Read <robin.g.read@gmail.com>
2023-02-25 16:18:12 +0000

    pypinball: Updated the draw_flipper() signature in the DisplayInterface method.

--- pypinball/display/display_interface.py
+++ pypinball/display/display_interface.py
@@ -36,8 +36,21 @@ class DisplayInterface(typing.Protocol):
             alpha (float): Alpha transparency. Values expected to be in the range [0, 1] where 1.0 means full opacity.
         """
 
-    def draw_flipper(self) -> None:
-        """Helper funcion to draw a flippper."""
+    def draw_flipper(
+        self,
+        pos: typing.Tuple[float, float],
+        angle: float,
+        size: typing.Tuple[float, float],
+        alpha: float,
+    ) -> None:
+        """Helper funcion to draw a flippper.
+
+        Args:
+            pos (tuple): Position of the center of the flipper in the format (x, y) both in pixel coordinates.
+            size (tuple): Size of the bumper in (width, height) format in pixel coordinates.
+            angle (float): Angle of the bumper.
+            alpha (float): Alpha transparency. Values expected to be in the range [0, 1] where 1.0 means full opacity.
+        """
 
     def draw_round_bumper(
         self,
