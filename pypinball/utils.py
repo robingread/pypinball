a8c0fb0e5cfef78ead9194fc999d31033b77de03
Robin Read <robin.g.read@gmail.com>
2023-02-25 17:48:36 +0000

    pypinball: Removed hard-coded ball radius value in the pypinball.utils.render_physics_ball() method.

--- pypinball/utils.py
+++ pypinball/utils.py
@@ -36,8 +36,7 @@ def render_physics_balls(
         display (DisplayInterface): Display to draw the balls onto.
     """
     for ball in balls:
-        # TODO: Address this hard-coded diameter.
-        display.draw_ball(pos=ball.position, diameter=30, alpha=1.0)
+        display.draw_ball(pos=ball.position, diameter=ball.radius, alpha=1.0)
 
 
 def render_physics_bumpers(
