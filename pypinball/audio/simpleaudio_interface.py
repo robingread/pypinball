782b3fd8adfdd4bba1f1eb9f5340843568ce02cb
Robin Read <robin.g.read@gmail.com>
2023-10-07 12:09:46 +0100

    pypinball: Adding a try/except around the playing of audio files in the SimpleAudio.play_sound_file() method. This is in the situations where there is a lower-level audio issues, for example with ALSA. Not playing audio shouldn't break the game!

--- pypinball/audio/simpleaudio_interface.py
+++ pypinball/audio/simpleaudio_interface.py
@@ -21,11 +21,20 @@ class SimpleAudio(AudioInterface):  # pylint: disable=too-few-public-methods
         logger.debug(f"Playing sound file: {file_path}")
 
         if not os.path.isfile(file_path):
-            logger.warning(f"Unknown find sound file: {file_path}")
+            logger.error(
+                f"Unknown find sound file: {file_path}, skipping playing audio"
+            )
             return False
 
         wave_obj = simpleaudio.WaveObject.from_wave_file(file_path)
-        play_obj = wave_obj.play()
+
+        try:
+            play_obj = wave_obj.play()
+        except (
+            simpleaudio._simpleaudio.SimpleaudioError  # pylint: disable=protected-access, c-extension-no-member
+        ) as exep:
+            logger.error(f"Failed to play audio file: {file_path}, exception: {exep}")
+            return False
 
         if self._blocking:
             play_obj.wait_done()
